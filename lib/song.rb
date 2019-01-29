87e4aee2f77a533b289b4317d765c6ffd4f42e20
Michael Soares <mikey.soares@gmail.com>
2019-01-29 18:15:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,6 +3,7 @@ require 'pry'
 class Song
 
   extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
   extend Findable::ClassMethods
   include Paramable::InstanceMethods
   attr_accessor :name
@@ -10,9 +11,9 @@ class Song
 
   @@songs = []
 
-  def initialize
-    @@songs << self
-  end
+  # def initialize
+  #   self.class.all << self
+  # end
 
   def self.all
     @@songs
