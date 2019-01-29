fa9127d3e42a99bc3943c17fe0528608c129c777
Michael Soares <mikey.soares@gmail.com>
2019-01-29 18:28:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,15 +5,15 @@ class Song
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
   include Paramable::InstanceMethods
-  # include Memorable::InstanceMethods
+  include Memorable::InstanceMethods
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
 
-  def initialize
-    @@songs << self
-  end
+  # def initialize
+  #   @@songs << self
+  # end
 
   def self.all
     @@songs
